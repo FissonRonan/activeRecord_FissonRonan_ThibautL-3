42ff407233723ac1d8eaad751b08c5a8d1dc11e0
Thibaut <louyot.thibaut@gmail.com>
2025-11-19 08:18:28 +0100

    [Modif] : Q2 du poly

--- src/activeRecord/PrincipaleJDBC.java
+++ src/activeRecord/PrincipaleJDBC.java
@@ -1,5 +1,7 @@
 package activeRecord;
 
+import connection.DBConnection;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,45 +16,12 @@ public class PrincipaleJDBC {
 
     public static void main(String[] args) {
 
-        // variables de connection
-        String userName = "root";
-        String password;
-        String serverName;
-        String portNumber;
-        String tableName;
-        String dbName;
-        if (System.getProperty("os.name").contains("Windows")) {
-            password = "";
-            serverName = "127.0.0.1";
-            //String portNumber = "3306";
-            portNumber = "3306"; // Port par défaut sur XAMPP
-            tableName = "personne";
 
-            // il faut une base nommee testPersonne !
-            dbName = "testpersonne";
-        } else {
-            password = "root";
-            serverName = "127.0.0.1";
-            //String portNumber = "3306";
-            portNumber = "8889"; // Port par défaut sur MAMP
-            tableName = "personne";
-
-            // il faut une base nommee testPersonne !
-            dbName = "testpersonne";
-        }
 
         try {
-            // chargement du driver jdbc
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            DBConnection dbconnection = DBConnection.getInstance();
 
-            // creation de la connection
-            Properties connectionProps = new Properties();
-            connectionProps.put("user", userName);
-            connectionProps.put("password", password);
-            String urlDB = "jdbc:mysql://" + serverName + ":";
-            urlDB += portNumber + "/" + dbName;
-            System.out.println(urlDB);
-            Connection connect = DriverManager.getConnection(urlDB, connectionProps);
+            Connection connect = dbconnection.getConnection();
             //Connection connect = DriverManager.getConnection("jdbc:mysql://db4free.net/testpersonne","scruzlara", "root2014");
             // creation de la table Personne
             String createString = "CREATE TABLE Personne ( "
@@ -154,9 +123,6 @@ public class PrincipaleJDBC {
 
         } catch (SQLException e) {
             System.out.println("*** ERREUR SQL ***");
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            System.out.println("*** ERREUR lors du chargement du driver ***");
             e.printStackTrace();
         } catch (Exception e) {
             System.out.println("*** ERREUR inconnue... ***");
